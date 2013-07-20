affc2ba53ebbfb5a9439cc962c42954fe1859d3d
suyesh Bhandari <suyeshb@gmail.com>
2013-07-20 14:12:28 -0400

    SOME TWEAKS

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'haml'
 gem 'activerecord'
 gem 'sinatra-activerecord'
 gem 'rake'
+gem 'rack-flash3', :git => "git://github.com/treeder/rack-flash.git"
 
 group :development do
   gem 'sqlite3'
--- bukbuk.rb
+++ bukbuk.rb
@@ -5,6 +5,20 @@ require 'bundler/setup'
 require 'sinatra/activerecord'
 configure(:development){ set :database, "sqlite3:///bukbuk.sqlite3" }
 require './models'
+require 'sinatra/base'
+require 'rack-flash'
+
+enable :sessions
+use Rack::Flash, :sweep => true
+
+set :sessions => true
+
+
+helpers do
+  def current_user
+    session[:user_id].nil? ? nil : User.find(session[:user_id])
+  end
+end
 
 
 get '/' do
@@ -12,6 +26,12 @@ get '/' do
 end
 
 post '/users/sign_up' do
+  User.create(params)
+  @users = User.all
+  redirect '/home'
+end
+
+post '/users/sign_in' do
   @user = User.where(:username => params[:username]).first
   if @user
     if @user.password == params[:password]
@@ -31,6 +51,7 @@ end
 
 
 get '/home' do
+	@tweets = Tweet.all
 	haml :home
 end
 
@@ -44,4 +65,16 @@ end
 
 get '/following' do
 	haml :following
+end
+
+post '/users/:id/tweets/new' do
+  @tweet = Tweet.create(:text => params[:tweet])
+  @user = User.find(params[:id])
+  @user.tweets << @tweet
+  redirect '/users/' + @user.id.to_s
+end
+
+get '/users/:id' do
+  @user = User.find(params[:id])
+  haml :profile
 end
